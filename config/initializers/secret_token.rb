05c8ba997b5cbbb0daf977f14d4527422d7618ea
小林 拓 <taku@kobayashi-no-MacBook-Air.local>
2013-11-02 15:26:41 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taptappun::Application.config.secret_token = 'd71d4470e7086d2628bf041804494d65f4093f7bd3f759655c1a3e064d916c5aed624fa15d10527d13731022b6690d663119ee3ff73306756e947608f7bdd9c0'
